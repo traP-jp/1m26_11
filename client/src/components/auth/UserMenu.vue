42e4160f1480fdc34c45e8d6ac03f598a810b835
shimeji_62 <yapou1919@gmail.com>
2026-08-10 07:51:47 +0900

    ログイン状態表示とログアウトメニューの作成

---
+++ client/src/components/auth/UserMenu.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import UserMenuButton from './UserMenuButton.vue'
+
+defineProps<{
+  displayName: string
+  logoutPending: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'logout'): void
+}>()
+
+const open = ref(false)
+</script>
+
+<template>
+  <div>
+    <UserMenuButton :displayName="displayName" :open="open" @toggle="open = !open" />
+
+    <div v-if="open">
+      <button type="button" :disabled="logoutPending" @click="emit('logout')">ログアウト</button>
+    </div>
+  </div>
+</template>
