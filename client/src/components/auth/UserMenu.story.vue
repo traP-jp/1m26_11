8d4811e8566b66b63407972d3e5bdc9213995c8d
shimeji_62 <yapou1919@gmail.com>
2026-08-11 13:20:32 +0900

    storyのついか

---
+++ client/src/components/auth/UserMenu.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import UserMenu from './UserMenu.vue'
+</script>
+
+<template>
+  <Story title="UserMenu">
+    <Variant title="Default">
+      <UserMenu displayName="Shimeji" :logoutPending="false" @logout="console.log('Logout clicked')" />
+    </Variant>
+  </Story>
+</template>
---
+++ client/src/components/auth/UserMenuButton.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import UserMenuButton from './UserMenuButton.vue'
+</script>
+
+<template>
+  <Story title="UserMenuButton">
+    <Variant title="Default">
+      <UserMenuButton displayName="Shimeji" :open="false" @toggle="console.log('Toggle clicked')" />
+    </Variant>
+  </Story>
+</template>
