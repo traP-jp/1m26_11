3d21789b719a703551b5c8ba75d85c15a57e8a5b
shimeji_62 <yapou1919@gmail.com>
2026-08-22 03:49:48 +0900

    RoomContextコンポーネントを作った

---
+++ client/src/components/auth/RoomContext.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps({
+  roomId: {
+    type: String,
+    required: true,
+  },
+  roomName: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex items-baseline space-x-3 px-4 py-2">
+    <span class="text-xs font-bold text-gray-800 tracking-wider"> ROOM {{ roomId }} </span>
+    <span class="text-base font-bold text-gray-800">
+      {{ roomName }}
+    </span>
+  </div>
+</template>
