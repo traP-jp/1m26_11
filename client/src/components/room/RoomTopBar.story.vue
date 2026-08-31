a9f4e1c4091e07a94fe1629afa62e84a4200f3e0
shimeji_62 <yapou1919@gmail.com>
2026-09-01 03:41:17 +0900

    RoomTopBarつくった＆RoomContext移動した

---
+++ client/src/components/room/RoomTopBar.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import RoomTopBar from './RoomTopBar.vue'
+import { gameTimerFixtures } from './GameTimer.fixture'
+</script>
+
+<template>
+  <Story title="Room/RoomTopBar">
+    <Variant title="デフォルト">
+      <div class="bg-gray-100 min-h-screen p-4">
+        <RoomTopBar
+          roomNumber="01"
+          roomName="最初の部屋"
+          :serverElapsedMs="gameTimerFixtures.active.serverElapsedMs"
+          :active="gameTimerFixtures.active.active"
+          @exit="() => console.log('退出処理が実行されましたわ！')"
+        />
+      </div>
+    </Variant>
+  </Story>
+</template>
