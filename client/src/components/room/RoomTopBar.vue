8627c399ceead4a89644b138fe9809d9abe107dc
shimeji_62 <yapou1919@gmail.com>
2026-09-01 03:44:43 +0900

    チェック通すの忘れてた

--- client/src/components/room/RoomTopBar.vue
+++ client/src/components/room/RoomTopBar.vue
@@ -21,29 +21,20 @@ function handleExit() {
 
 <template>
   <header class="flex items-center justify-between w-full px-6 py-4 bg-white shadow-md rounded-2xl">
-    
     <div class="flex-1 flex justify-start items-center">
-      <RoomContext 
-        :roomNumber="roomNumber" 
-        :roomName="roomName" 
+      <RoomContext
+        :roomNumber="roomNumber"
+        :roomName="roomName"
         class="transform scale-100 origin-left"
       />
     </div>
 
     <div class="flex-1 flex justify-center items-center">
-      <GameTimer 
-        :serverElapsedMs="serverElapsedMs" 
-        :active="active" 
-        class="transform scale-100"
-      />
+      <GameTimer :serverElapsedMs="serverElapsedMs" :active="active" class="transform scale-100" />
     </div>
 
     <div class="flex-1 flex justify-end items-center">
-      <ExitButton 
-        @exit="handleExit" 
-        class="transform scale-100 origin-right"
-      />
+      <ExitButton @exit="handleExit" class="transform scale-100 origin-right" />
     </div>
-
   </header>
 </template>
