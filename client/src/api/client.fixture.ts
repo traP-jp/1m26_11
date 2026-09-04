435fc9a1801078608001f31dddae683b399510ca
sahara2006 <shouta.04077@gmail.com>
2026-09-05 01:26:11 +0900

    waqran2

--- client/src/api/client.fixture.ts
+++ client/src/api/client.fixture.ts
@@ -24,6 +24,7 @@ export function createFixtureApiClient(): ApiClient {
     startOrResumeRun: unsupported,
     getCurrentRun: unsupported,
     getProblem: unsupported,
+    getProblemAssets: unsupported,
     submitQuery: unsupported,
     submitAnswer: unsupported,
   }
